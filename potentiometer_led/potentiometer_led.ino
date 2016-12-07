48e70fb2b1b025115182245f2c582e3695395f45
JoshuaSeiple <jseiple@bancroftschool.org>
2016-12-07 10:46:01 -0500

    blinking lights

---
+++ potentiometer_led/potentiometer_led.ino
@@ -0,0 +1,16 @@
+int sensorPin = 0;
+int ledPin = 13;
+
+void setup() {
+pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+
+  digitalWrite(ledPin, HIGH);
+  delay(sensorValue);
+  digitalWrite(ledPin, LOW);
+  delay(sensorValue);
+
+}
